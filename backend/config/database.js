f0c9cf8367fefcb9aabaa066cf265901e753f0da
Giorgos-Mitrakos <giorgos_mitrakos@yahoo.com>
2022-11-18 22:24:48 +0200

    fix database

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'magnetmarket_db'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
+      database: env('DATABASE_NAME', 'f127527kko_strapi'),
+      user: env('DATABASE_USERNAME', 'f127527kko'),
+      password: env('DATABASE_PASSWORD', 'Z?d94oX!_S'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
